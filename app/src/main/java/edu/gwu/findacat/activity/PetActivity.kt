478fe64461eec47414a900ec3875bac033ef0daf
KentHeDJ <hedejian_kent@163.com>
2018-10-29 10:10:39 -0400

    Location and UI enhancement

--- app/src/main/java/edu/gwu/findacat/activity/PetActivity.kt
+++ app/src/main/java/edu/gwu/findacat/activity/PetActivity.kt
@@ -1,6 +1,8 @@
 package edu.gwu.findacat.activity
 
 import android.content.Intent
+import android.location.Location
+import android.location.LocationListener
 import android.os.Bundle
 import android.support.v4.content.ContextCompat.startActivity
 import android.support.v7.app.AlertDialog
@@ -11,24 +13,28 @@ import android.view.Menu
 import android.view.MenuItem
 import android.view.View
 import android.widget.EditText
-import edu.gwu.findacat.PersistenceManager
-import edu.gwu.findacat.PetSearchManager
-import edu.gwu.findacat.PetsAdapter
-import edu.gwu.findacat.R
+import edu.gwu.findacat.*
 import edu.gwu.findacat.R.id.recycle_view
 import edu.gwu.findacat.R.string.name
 import edu.gwu.findacat.activity.PetActivity.Companion.CAT_DATA_KEY
 import edu.gwu.trivia.model.generated.petfinder.PetItem
 import kotlinx.android.synthetic.main.activity_pet.*
+import android.location.Geocoder
+import android.support.v7.widget.GridLayoutManager
+import org.jetbrains.anko.toast
+import java.util.*
 
-class PetActivity : AppCompatActivity(), PetSearchManager.PetSearchCompletionListener, PetsAdapter.OnItemClickListener {
+
+class PetActivity : AppCompatActivity(), PetSearchManager.PetSearchCompletionListener, PetsAdapter.OnItemClickListener, LocationDetector.LocationListener {
 
     private val TAG="PetAcitivity"
     private lateinit var petSearchManager: PetSearchManager
     private lateinit var petsAdapter: PetsAdapter
     private lateinit var persistenceManager: PersistenceManager
+    private lateinit var locationDetector: LocationDetector
     private var flag = 0
-    var zip = 90210
+    var zipUsed = 22202
+
 
     companion object {
         val CAT_DATA_KEY = "catData"
@@ -39,21 +45,29 @@ class PetActivity : AppCompatActivity(), PetSearchManager.PetSearchCompletionLis
         setContentView(R.layout.activity_pet)
 
         setSupportActionBar(pet_toolbar)
-        supportActionBar?.title = "Nearby Cats"
+
+        locationDetector = LocationDetector(this)
+        locationDetector.locationListener = this
 
         persistenceManager = PersistenceManager(this)
 
         flag = intent.getIntExtra(MenuActivity.FLAG_KEY, 0)
         if (flag == 1) {
+            supportActionBar?.title = "Favorite Cats"
+
             petsAdapter = PetsAdapter(persistenceManager.fetchFavorites(), this)
-            recycle_view.layoutManager = LinearLayoutManager(this)
+            recycle_view.layoutManager = GridLayoutManager(this, 2)
             recycle_view.adapter = petsAdapter
         }
         else {
 
+            supportActionBar?.title = "Nearby Cats"
+
+            locationDetector.detectLocation()
+
             petSearchManager = PetSearchManager()
             petSearchManager.petSearchCompletionListener = this
-            petSearchManager.searchPets(zip)
+            petSearchManager.searchPets(zipUsed)
         }
 
 
@@ -84,11 +98,11 @@ class PetActivity : AppCompatActivity(), PetSearchManager.PetSearchCompletionLis
                 dialog, whichButton ->
                 //showMessage("display the game score or anything!")
                 var zipString = editTextZip!!.text.toString()
-                zip = zipString.toInt()
+                zipUsed = zipString.toInt()
 
                 petSearchManager = PetSearchManager()
                 petSearchManager.petSearchCompletionListener = this@PetActivity
-                petSearchManager.searchPets(zip)
+                petSearchManager.searchPets(zipUsed)
 
                 dialog.dismiss()
 
@@ -112,7 +126,7 @@ class PetActivity : AppCompatActivity(), PetSearchManager.PetSearchCompletionLis
 
     override fun petsLoaded(petItems: List<PetItem>) {
         petsAdapter = PetsAdapter(petItems, this)
-        recycle_view.layoutManager = LinearLayoutManager(this)
+        recycle_view.layoutManager = GridLayoutManager(this, 2)
         recycle_view.adapter = petsAdapter
     }
 
@@ -123,5 +137,24 @@ class PetActivity : AppCompatActivity(), PetSearchManager.PetSearchCompletionLis
         val detailIntent = Intent(this@PetActivity, PetDetailActivity::class.java)
         detailIntent.putExtra(CAT_DATA_KEY, petItem)
         startActivity(detailIntent)
+    }
+
+    override fun locationFound(location: Location) {
+        val geocoder = Geocoder(this, Locale.getDefault())
+        val addresses = geocoder.getFromLocation(location.latitude, location.longitude, 1)
+        val zip = addresses[0].postalCode.toInt()
+        zipUsed = zip
+
+        petSearchManager = PetSearchManager()
+        petSearchManager.petSearchCompletionListener = this
+        petSearchManager.searchPets(zipUsed)
+    }
+
+    override fun locationNotFound(reason: LocationDetector.FailureReason) {
+        when (reason) {
+            LocationDetector.FailureReason.TIMEOUT -> toast(getString(R.string.location_not_found))
+            LocationDetector.FailureReason.NO_PERMISSION -> toast(getString(R.string.no_location_permission))
+        }
+
     }
 }
